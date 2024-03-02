d53b7b900d507291ebe3eb4c07cb9489ac96e972
Martin-Roberto-lecuona <robertlecuona@gmail.com>
2024-03-02 16:42:44 -0300

    nav bar on top y cambio de logo

--- src/Componentes/Navbar.js
+++ src/Componentes/Navbar.js
@@ -1,38 +1,80 @@
 import React from 'react'
 import { Link } from 'react-scroll'
 import styles from './Navbar.module.css'
-import { useState } from 'react'
-import { IoHomeSharp } from "react-icons/io5";
-import { FaUserCircle } from "react-icons/fa";
-import { TbUsersGroup } from "react-icons/tb";
-import { IoSettingsSharp } from "react-icons/io5";
+import { useState, useEffect } from 'react';
+import { IoHomeSharp } from "react-icons/io5"
+import { FaUserCircle } from "react-icons/fa"
+import { TbUsersGroup } from "react-icons/tb"
+import { IoSettingsSharp } from "react-icons/io5"
+
+import logoLomt from '../LoMT-logo.png';
+
 
 const Navbar = () => {
-    const [navBarOpen, setNavBarOpen] = useState(true)
-    const links=[
-        {id:1,path:'./Home',link:"Home", logo:<IoHomeSharp />},  
-        {id:2, path:'./Profile', link:"Profile",logo:<FaUserCircle />}, 
-        {id:3, path:'./Clients', link:"Clients",logo:<TbUsersGroup />},
-        {id:4, path:'./Settings', link:"Settings",logo:<IoSettingsSharp />}
-    ]
-  return (
-    <div className={navBarOpen ?  styles.navBarOpen : styles.navBar}>
-        {navBarOpen && <p className={styles.titleLogo}>LoMt | fttd software solutions </p>}
-        <ul>
-            {links.map(x => (
-                <div>                
-                    <Link
-                    onClick={() => setNavBarOpen(false)}
-                    to={x.link}
-                    smooth
-                    duration={500}
-                    className = {styles.navBarLink}>
-                        {x.logo} {navBarOpen &&  <span className={styles.hideOnMobile}>{x.link}</span>}
-                    </Link>
-                </div>
-            ))}
-        </ul> 
+	const [navBarOpen, setNavBarOpen] = useState(false)
+	const [navBarOnTop, setNavBarOnTop] = useState(false)
+	const [windowDimension, setWindowDimension] = useState({
+			with : window.innerWidth,
+			height: window.innerHeight
+		})
 
+	const links=[
+			{id:1   , path:'./Home'     , link:"Home"       , logo:<IoHomeSharp size={24}/>},
+			{id:2   , path:'./Profile'  , link:"Profile"    , logo:<FaUserCircle size={24}/>},
+			{id:3   , path:'./Clients'  , link:"Clients"    , logo:<TbUsersGroup size={24}/>},
+			{id:4   , path:'./Settings' , link:"Settings"   , logo:<IoSettingsSharp size={24}/>}
+	]
+
+
+
+  const handleLinkHover = (e) => {
+    if (!navBarOnTop)
+			setNavBarOpen(true)
+  }
+  const handleLinkLeave = (e) => {
+    if (!navBarOnTop)
+			setNavBarOpen(false)
+  }
+
+  const detectDimension = () => {
+		setWindowDimension({
+			with : window.innerWidth,
+			height: window.innerHeight
+		})
+	}
+
+  useEffect(() => {
+    windowDimension.with < 650 ?  setNavBarOnTop(true) : setNavBarOnTop(false)
+  }, []);
+
+	useEffect(() => {
+    window.addEventListener('resize', detectDimension)
+		windowDimension.with < 650 ?  setNavBarOnTop(true) && setNavBarOpen(false): setNavBarOnTop(false)
+    return () => {window.removeEventListener('resize', detectDimension)}
+  }, [windowDimension]);
+
+
+  return (
+    <div className={!navBarOnTop ? (navBarOpen ?  styles.navBarOpen : styles.navBar) : styles.navBarOnTop}
+			onMouseOver={handleLinkHover} onMouseLeave={handleLinkLeave}>
+			
+			{navBarOpen && <img  src={logoLomt} height={'150px'}/>}
+			{!navBarOpen && !navBarOnTop && <p className={styles.logo}><img src={logoLomt} height={'90px'}/></p>}
+			{navBarOnTop && <p className={styles.logo}><img src={logoLomt} height={'80px'}/></p>}
+
+			<ul className = {navBarOnTop ? styles.LinkContainerTop : styles.LinkContainer}> 
+				{links.map(x => (
+					<div>
+						<Link
+						to={x.link}
+						smooth
+						duration={100}
+						className = {navBarOnTop ? styles.navBarLinkTop : styles.navBarLink}>
+							{x.logo}  {navBarOpen && <span>{x.link}</span>}
+						</Link>
+					</div>
+				))}
+			</ul>
     </div>
   )
 }
