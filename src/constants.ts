a6f2e6f645e515296845883030b7de8624deb278
Martin-Roberto-lecuona <robertlecuona@gmail.com>
2024-05-16 16:03:59 -0300

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 export const USER_REGEX : RegExp = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
 export const PASS_REGEX : RegExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!'#$%]).{8,24}$/
 export const APILINK    : string= 'https://dummyjson.com/users'
-export const USERSLINK    : string= 'http://127.0.0.1:8000/users'
+export const USERSLINK    : string= 'http://192.168.0.62/users'
 // export const USERSLINK    : string= 'http://0.0.0.0:8000/users'
 
 export type  color      = 'fondo'|'claro'|'oscuro'|'basico';
